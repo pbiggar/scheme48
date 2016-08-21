616fa86c6039004644f6240620446342edbeeb9f
Paul Biggar <paul.biggar@gmail.com>
2016-08-21 01:30:21 -0400

    Add more symbol functions

--- src/Builtins.hs
+++ src/Builtins.hs
@@ -3,38 +3,50 @@ module Builtins (builtins) where
 import AST
 
 builtins :: [(String, ([LispVal] -> LispVal))]
-builtins = [
-    ("+", numericBinop (+))
+builtins = numbers ++ symbols ++ strings
+
+
+numbers = [
+    ("number?", isNumber)
+  , ("+", numericBinop (+))
   , ("-", numericBinop (-))
   , ("*", numericBinop (*))
   , ("/", numericBinop div)
   , ("mod", numericBinop mod)
   , ("quotient", numericBinop quot)
-  , ("remainder", numericBinop rem)
-  , ("string?", isString)
-  , ("number?", isNumber)
-  , ("symbol?", isSymbol)]
-
-isSymbol :: [LispVal] -> LispVal
-isSymbol [(Atom _)] = Bool True
-isSymbol _ = Bool False
-
-isString :: [LispVal] -> LispVal
-isString [(String _)] = Bool True
-isString _ = Bool False
+  , ("remainder", numericBinop rem)]
 
 isNumber :: [LispVal] -> LispVal
 isNumber [(Number n)] = Bool True
 isNumber _ = Bool False
-
 
 numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
 numericBinop op params = Number $ foldl1 op $ map unpackNum params
 
 unpackNum :: LispVal -> Integer
 unpackNum (Number n) = n
--- unpackNum (String s) = let parsed = reads s :: [(Integer, String)] in
---                           if null parsed
---                             then 0
---                             else fst . head $ parsed
 unpackNum _ = 0
+
+
+
+strings = [("string?", isString)]
+
+isString :: [LispVal] -> LispVal
+isString [(String _)] = Bool True
+isString _ = Bool False
+
+
+
+symbols = [ ("symbol?", isSymbol)
+          , ("string->symbol", string2symbol)
+          , ("symbol->string", symbol2string)]
+
+isSymbol :: [LispVal] -> LispVal
+isSymbol [(Atom _)] = Bool True
+isSymbol _ = Bool False
+
+symbol2string :: [LispVal] -> LispVal
+symbol2string [(Atom s)] = String s
+
+string2symbol :: [LispVal] -> LispVal
+string2symbol [(String s)] = Atom s
