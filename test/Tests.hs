6c262b5c13bdad0e58074b128b4daa98969c4304
Paul Biggar <paul.biggar@gmail.com>
2016-08-22 15:05:46 -0400

    Add more tests from course

--- test/Tests.hs
+++ test/Tests.hs
@@ -7,8 +7,9 @@ import System.Exit
 
 import qualified Runner
 
+et :: String -> String -> SpecWith ()
 et l r = do
-  it ("should evaluate " ++ l ++ " to " ++ r) $ do
+  it ("[" ++ l ++ " == " ++ r ++ "]") $ do
     (result, _) <- capture $ Runner.run l
     result `shouldBe` r
 
@@ -20,3 +21,14 @@ main = do
       et "(symbol? 'a)" "#t"
       et "(string? asd)" "#f"
       et "(- (+ 4 6 3) 3 5 2)" "3"
+      et "(+ 2 \"two\")" "Invalid type: expected Int, found String \"two\""
+      et "(+ 2)" "Expected 2 args; found values 2"
+      et "(what? 2)" "Unrecognized primitive function: \"what?\""
+      et "(+ 2 2)" "4"
+      et "(+ 2 (- 4 1))" "5"
+      et "(- (+ 4 6 3) 3 5 2)" "3"
+      et "'atom" "atom"
+      et "2" "2"
+      et "\"a string\"" "\"a string\""
+      et "(a '(imbalanced parens)"
+       "Parse error at \"lisp\" (line 1, column 24):\nunexpected end of input\nexpecting space or \")\""
