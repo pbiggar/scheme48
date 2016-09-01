f1a8ba2e16b4b8f65564cd6f2ef283dcc8fc05df
Paul Biggar <paul.biggar@gmail.com>
2016-09-01 17:03:25 -0400

    add a repl

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,15 @@
 module Main where
 
-import System.Environment as Env
-import System.Exit as Exit
+import qualified System.Environment as Env
+import qualified System.Exit as Exit
 
 import qualified Runner
 
 main :: IO ()
 main = do
-  (expr:_) <- Env.getArgs
-  Runner.run expr
-  Exit.exitSuccess
+        args <- Env.getArgs
+        case length args of
+          0 -> Runner.runRepl
+          1 -> Runner.run $ args !! 0
+          otherwise -> putStrLn "Program takes only 0 or 1 argument"
+        Exit.exitSuccess
--- src/Runner.hs
+++ src/Runner.hs
@@ -1,11 +1,12 @@
-module Runner (run) where
+module Runner (run, runRepl) where
 
-import Control.Monad
+import           Control.Monad (liftM)
+import qualified System.IO as IO
 
-import Parser (readExpr)
-import Interpreter (eval)
-import PrettyPrinter (showVal)
-import Errors (extractValue, trapError)
+import qualified Parser
+import qualified Interpreter
+import qualified PrettyPrinter
+import qualified Errors
 
 
 run :: String -> IO ()
@@ -15,7 +16,7 @@ run source = do
     then putStrLn $ "Args are:  " ++ source
     else return ()
 
-  parsed <- return $ readExpr source
+  parsed <- return $ Parser.readExpr source
   if debug
     then do
 --       putStrLn $ "Parsed:    " ++ (extractValue $ trapError parsed)
@@ -23,5 +24,33 @@ run source = do
     else return ()
 
 
-  evaled <- return $ liftM showVal $ parsed >>= eval
-  putStr $ extractValue $ trapError evaled
+  evaled <- return $ liftM PrettyPrinter.showVal $ parsed >>= Interpreter.eval
+  putStr $ Errors.extractValue $ Errors.trapError evaled
+
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> IO.hFlush IO.stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr =
+  return $ Errors.extractValue
+         $ Errors.trapError
+            (liftM PrettyPrinter.showVal $ Parser.readExpr expr >>= Interpreter.eval)
+
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+   result <- prompt
+   if pred result
+      then return ()
+      else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
